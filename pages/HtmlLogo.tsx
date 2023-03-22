9ef5c8292d4ca60c748ca53ce0d6bbfd75656d49
cum-monster <114361297+cum-monster@users.noreply.github.com>
2023-03-22 15:45:20 +0100

    Delete HtmlLogo.tsx

--- pages/HtmlLogo.tsx
+++
@@ -1,8 +0,0 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHtml5 } from '@fortawesome/free-brands-svg-icons'
-
-const HtmlLogo = () => {
-  return <FontAwesomeIcon icon={faHtml5} />
-}
-
-export default HtmlLogo
