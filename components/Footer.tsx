b36c83ca5b40e39f882be97a9f636b45305ba061
cum-monster <114361297+cum-monster@users.noreply.github.com>
2023-03-22 09:02:57 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <div className="max-w-screen-xl px-4 py-12 mx-auto mt-auto space-y-8 overflow-hidden sm:px-6 lg:px-3 ">
       <p className="mt-8 text-base leading-6 text-center text-neutral-200">
-        © {new Date().getFullYear()} XSOMENOOB
+        © {new Date().getFullYear()} xsomenoob
       </p>
     </div>
   );
