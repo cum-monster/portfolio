13b1a461ffb8884ee12611c419143910a07f0ab6
cum-monster <114361297+cum-monster@users.noreply.github.com>
2023-03-22 15:17:45 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home() {
             <div className="flex items-center justify-between w-full">
               <div className="flex justify-center items-center h-[26px]">
                 <Image
-                  src="https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg"
+                  src="steam.png"
                   className="rounded-sm"
                   width="24"
                   height="24"
