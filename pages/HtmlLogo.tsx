9ef59e1227abd9f782c31c96831db3ecbef27c78
cum-monster <114361297+cum-monster@users.noreply.github.com>
2023-03-22 09:14:47 +0100

    Create HtmlLogo.tsx

---
+++ pages/HtmlLogo.tsx
@@ -0,0 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHtml5 } from '@fortawesome/free-brands-svg-icons'
+
+const HtmlLogo = () => {
+  return <FontAwesomeIcon icon={faHtml5} />
+}
+
+export default HtmlLogo
